b354901f5f2156b121e056f42b65ea13414d9236
Praveen <devpraveenkr@gmail.com>
2024-12-20 16:54:58 +0530

    Employee CRUD updated

---
+++ spring/src/main/java/com/codingmstr/crud/employee/Employee.java
@@ -0,0 +1,19 @@
+package com.codingmstr.crud.employee;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String department;
+    private Double salary;
+}
